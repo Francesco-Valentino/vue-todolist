5f7b223e171a1b8ef053075d07de8dce350966d6
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-06 15:44:23 +0200

    created to do list array of objects

--- js/script.js
+++ js/script.js
@@ -3,7 +3,79 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            
+            todoList: [
+                {
+                    text: "Seguire la lezione mattutina",
+                    done: false
+                },
+
+                {
+                    text: "Preparare il pranzo",
+                    done: false
+                },
+
+
+                {
+                    text: "Pranzare",
+                    done: false
+                },
+
+                {
+                    text: "Completare l'esercizio",
+                    done: false
+                },
+
+                {
+                    text: "Fare la spesa",
+                    done: false
+                },
+
+                {
+                    text: "Fare la lavatrice",
+                    done: false
+                },
+
+                {
+                    text: "Ponderare la sfera",
+                    done: false
+                },
+
+                {
+                    text: "Lavare il pavimento",
+                    done: false
+                },
+
+                {
+                    text: "Preparare la cena",
+                    done: false
+                },
+
+                {
+                    text: "Cenare",
+                    done: false
+                },
+
+                {
+                    text: "Venerare il dio-macchina",
+                    done: false
+                },
+
+                {
+                    text: "Prepararsi per andare a dormire",
+                    done: false
+                },
+
+                {
+                    text: "Leggere un libro",
+                    done: false
+                },
+
+                {
+                    text: "Addormentarsi",
+                    done: false
+                },
+
+            ]
         }
     },
 
